58dcda7be9504da2266d2b45d9c7a2b12adf06c6
Ahad726 <atmahad726@gmail.com>
2021-06-10 05:45:00 +0600

    creating projects

---
+++ Rococo.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Rococo.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ Rococo.Utility/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Rococo.Utility
+{
+    public class Class1
+    {
+    }
+}
