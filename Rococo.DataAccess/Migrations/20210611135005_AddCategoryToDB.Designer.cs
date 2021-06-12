befa11263b3505a6c1f8a1365440e9fba8cd5a6a
Ahad726 <atmahad726@gmail.com>
2021-06-12 16:12:15 +0600

    Generic Repository added

--- Rococo.DataAccess/Migrations/20210611135005_AddCategoryToDB.Designer.cs
+++ Rococo.DataAccess/Migrations/20210611135005_AddCategoryToDB.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rococo.DataAccess.Data;
 
 namespace Rococo.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210611135005_AddCategoryToDB")]
+    partial class AddCategoryToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
