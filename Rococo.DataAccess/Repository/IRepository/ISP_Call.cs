2b8b7534f21b4388fcc7728a16ee035f0d556b4a
Ahad726 <atmahad726@gmail.com>
2021-06-12 17:51:12 +0600

    Store procedure interface added

---
+++ Rococo.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -0,0 +1,16 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rococo.DataAccess.Repository.IRepository
+{
+    public interface ISP_Call : IDisposable
+    {
+        T Single<T>(string ProcedureName, DynamicParameters pram = null);
+        void Execute(string ProcedureName, DynamicParameters pram = null);
+        T OneRecode<T>(string ProcedureName, DynamicParameters pram = null);
+        IEnumerable<T> List<T>(string ProcedureName, DynamicParameters pram = null);
+        Tuple<IEnumerable<T1>,IEnumerable<T2>> List<T1,T2>(string ProcedureName, DynamicParameters pram = null);
+    }
+}
