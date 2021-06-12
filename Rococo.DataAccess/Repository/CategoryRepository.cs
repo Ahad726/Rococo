e2bdb2803545d939230ea10dfbb6b2ca26e36b29
Ahad726 <atmahad726@gmail.com>
2021-06-12 17:10:02 +0600

    Category Reository added

---
+++ Rococo.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Rococo.DataAccess.Data;
+using Rococo.DataAccess.Repository.IRepository;
+using Rococo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rococo.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext)
+            :base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Update(Category category)
+        {
+            var categoryFromDb = _dbContext.Categories.FirstOrDefault(x => x.Id == category.Id);
+
+            if (categoryFromDb != null)
+                categoryFromDb.Name = category.Name;
+            _dbContext.SaveChanges();
+            
+        }
+    }
+}
