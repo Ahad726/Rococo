de172819bca115e39fd4d247550638b3a521a8af
Ahad726 <atmahad726@gmail.com>
2021-06-17 18:18:55 +0600

    Default IdentityUser Extended

--- Rococo.DataAccess/Migrations/20210617120142_AddPropertiesTOUserDB.Designer.cs
+++ Rococo.DataAccess/Migrations/20210617120142_AddPropertiesTOUserDB.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rococo.DataAccess.Data;
 
 namespace Rococo.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210617120142_AddPropertiesTOUserDB")]
+    partial class AddPropertiesTOUserDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
